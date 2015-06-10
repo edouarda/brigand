40ff2fcaa650e818dab496d9a418c9fd001a84f3
Edouard Alligand <noone@prout.org>
2015-06-10 23:11:01 +0200

    Removed nonsensical T...

--- brigand/map.hpp
+++ brigand/map.hpp
@@ -19,50 +19,39 @@ namespace brigand
         static no_such_type_ at(U) {}     
     };
 
-    template <typename Last>
-    struct map<Last>
-    {
-    
-        static typename Last::second_type at(typename Last::first_type);
-    
-        template <typename U>
-        static no_such_type_ at(U) {}
-    
-    };
-
     // fastlane for constant amortized time
-    template <class T0, class... T>
-    struct map<T0, T...>
+    template <class T0>
+    struct map<T0>
     {
         static typename T0::second_type at(typename T0::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
-    template <class T0, class T1, class... T>
-    struct map<T0, T1, T...>
+    template <class T0, class T1>
+    struct map<T0, T1>
     {
         static typename T0::second_type at(typename T0::first_type);
         static typename T1::second_type at(typename T1::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
-    template <class T0, class T1, class T2, class... T>
-    struct map<T0, T1, T2, T...>
+    template <class T0, class T1, class T2>
+    struct map<T0, T1, T2>
     {
         static typename T0::second_type at(typename T0::first_type);
         static typename T1::second_type at(typename T1::first_type);
         static typename T2::second_type at(typename T2::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
-    template <class T0, class T1, class T2, class T3, class... T>
-    struct map<T0, T1, T2, T3, T...>
+    template <class T0, class T1, class T2, class T3>
+    struct map<T0, T1, T2, T3>
     {
         static typename T0::second_type at(typename T0::first_type);
         static typename T1::second_type at(typename T1::first_type);
@@ -70,11 +59,11 @@ namespace brigand
         static typename T3::second_type at(typename T3::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
-    template <class T0, class T1, class T2, class T3, class T4, class... T>
-    struct map<T0, T1, T2, T3, T4, T...>
+    template <class T0, class T1, class T2, class T3, class T4>
+    struct map<T0, T1, T2, T3, T4>
     {
         static typename T0::second_type at(typename T0::first_type);
         static typename T1::second_type at(typename T1::first_type);
@@ -83,11 +72,11 @@ namespace brigand
         static typename T4::second_type at(typename T4::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
-    template <class T0, class T1, class T2, class T3, class T4, class T5, class... T>
-    struct map<T0, T1, T2, T3, T4, T5, T...>
+    template <class T0, class T1, class T2, class T3, class T4, class T5>
+    struct map<T0, T1, T2, T3, T4, T5>
     {
         static typename T0::second_type at(typename T0::first_type);
         static typename T1::second_type at(typename T1::first_type);
@@ -97,11 +86,11 @@ namespace brigand
         static typename T5::second_type at(typename T5::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
-    template <class T0, class T1, class T2, class T3, class T4, class T5, class T6, class... T>
-    struct map<T0, T1, T2, T3, T4, T5, T6, T...>
+    template <class T0, class T1, class T2, class T3, class T4, class T5, class T6>
+    struct map<T0, T1, T2, T3, T4, T5, T6>
     {
         static typename T0::second_type at(typename T0::first_type);
         static typename T1::second_type at(typename T1::first_type);
@@ -112,7 +101,7 @@ namespace brigand
         static typename T6::second_type at(typename T6::first_type);
     
         template <typename U>
-        static decltype(typename map<T...>::at(U{})) at(U);
+        static no_such_type_ at(U) {}
     };
 
     template <class T0, class T1, class T2, class T3, class T4, class T5, class T6, class T7, class... T>
