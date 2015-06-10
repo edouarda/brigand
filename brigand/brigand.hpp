f0fc4a46ac8186eb307391734a1db21c6aff2b4b
Joel Falcou <joel.falcou@lri.fr>
2015-06-10 19:12:12 +0200

    Added central brigand.hpp header

---
+++ brigand/brigand.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <brigand/append.hpp>
+#include <brigand/apply.hpp>
+#include <brigand/arithmetic.hpp>
+#include <brigand/at.hpp>
+#include <brigand/back.hpp>
+#include <brigand/bool.hpp>
+#include <brigand/comparisons.hpp>
+#include <brigand/count.hpp>
+#include <brigand/for_each.hpp>
+#include <brigand/for_each_args.hpp>
+#include <brigand/front.hpp>
+#include <brigand/identity.hpp>
+#include <brigand/list.hpp>
+#include <brigand/logical.hpp>
+#include <brigand/size.hpp>
+#include <brigand/transform.hpp>
+#include <brigand/adapted/pair.hpp>
+#include <brigand/adapted/tuple.hpp>
+#include <brigand/adapted/variant.hpp>
