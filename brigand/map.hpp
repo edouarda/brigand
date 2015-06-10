024ebcb23a8237f1e56ad10ab1e838b522a8cb56
Edouard Alligand <noone@prout.org>
2015-06-10 22:33:45 +0200

    Now with fast lane

--- brigand/map.hpp
+++ brigand/map.hpp
@@ -25,20 +25,114 @@ namespace brigand
     
         static typename Last::second_type at(typename Last::first_type);
     
-        // I wanted to call it "not sure if type" but Joel didn't want to :(
         template <typename U>
         static no_such_type_ at(U) {}
     
     };
 
-    template <class First, class... T>
-    struct map<First, T...>
+    // fastlane for constant amortized time
+
+    template <class T0, class... T>
+    struct map<T0, T...>
     {
-        static typename First::second_type at(typename First::first_type);
+        static typename T0::second_type at(typename T0::first_type);
     
         template <typename U>
         static decltype(typename map<T...>::at(U{})) at(U);
     };
+
+    template <class T0, class T1, class... T>
+    struct map<T0, T1, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+    template <class T0, class T1, class T2, class... T>
+    struct map<T0, T1, T2, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+        static typename T2::second_type at(typename T2::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+    template <class T0, class T1, class T2, class T3, class... T>
+    struct map<T0, T1, T2, T3, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+        static typename T2::second_type at(typename T2::first_type);
+        static typename T3::second_type at(typename T3::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+    template <class T0, class T1, class T2, class T3, class T4, class... T>
+    struct map<T0, T1, T2, T3, T4, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+        static typename T2::second_type at(typename T2::first_type);
+        static typename T3::second_type at(typename T3::first_type);
+        static typename T4::second_type at(typename T4::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+    template <class T0, class T1, class T2, class T3, class T4, class T5, class... T>
+    struct map<T0, T1, T2, T3, T4, T5, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+        static typename T2::second_type at(typename T2::first_type);
+        static typename T3::second_type at(typename T3::first_type);
+        static typename T4::second_type at(typename T4::first_type);
+        static typename T5::second_type at(typename T5::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+    template <class T0, class T1, class T2, class T3, class T4, class T5, class T6, class... T>
+    struct map<T0, T1, T2, T3, T4, T5, T6, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+        static typename T2::second_type at(typename T2::first_type);
+        static typename T3::second_type at(typename T3::first_type);
+        static typename T4::second_type at(typename T4::first_type);
+        static typename T5::second_type at(typename T5::first_type);
+        static typename T6::second_type at(typename T6::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+    template <class T0, class T1, class T2, class T3, class T4, class T5, class T6, class T7, class... T>
+    struct map<T0, T1, T2, T3, T4, T5, T6, T7, T...>
+    {
+        static typename T0::second_type at(typename T0::first_type);
+        static typename T1::second_type at(typename T1::first_type);
+        static typename T2::second_type at(typename T2::first_type);
+        static typename T3::second_type at(typename T3::first_type);
+        static typename T4::second_type at(typename T4::first_type);
+        static typename T5::second_type at(typename T5::first_type);
+        static typename T6::second_type at(typename T6::first_type);
+        static typename T7::second_type at(typename T7::first_type);
+    
+        template <typename U>
+        static decltype(typename map<T...>::at(U{})) at(U);
+    };
+
+
 
 
     template <typename M, typename K>
--- test/map_test.cpp
+++ test/map_test.cpp
@@ -9,6 +9,38 @@ static_assert(std::is_same<brigand::lookup<brigand::map<>, int>::type, brigand::
 
 using map_test = brigand::map<brigand::pair<int, bool>, brigand::pair<char, int>>;
 
-static_assert(std::is_same<brigand::lookup<map_test, int>::type, int>::value, "should be bool");
+static_assert(std::is_same<brigand::lookup<map_test, int>::type, bool>::value, "should be bool");
 static_assert(std::is_same<brigand::lookup<map_test, char>::type, int>::value, "should be int");
 static_assert(std::is_same<brigand::lookup<map_test, bool>::type, brigand::no_such_type_>::value, "should be not found");
+
+struct type_one {};
+struct type_two {};
+struct type_three {};
+struct type_four {};
+struct type_five {};
+struct type_six {};
+struct type_seven {};
+struct type_eight {};
+struct type_nine {};
+
+using big_map = brigand::map<
+    brigand::pair<type_one, int>,
+    brigand::pair<type_two, type_one>,
+    brigand::pair<type_three, type_two>,
+    brigand::pair<type_four, type_three>,
+    brigand::pair<type_five, type_four>,
+    brigand::pair<type_six, type_five>,
+    brigand::pair<type_seven, type_six>,
+    brigand::pair<type_eight, type_seven>,
+    brigand::pair<type_nine, type_eight>>;
+
+static_assert(std::is_same<brigand::lookup<big_map, type_one>::type, int>::value,                  "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_two>::type, type_one>::value,             "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_three>::type, type_two>::value,           "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_four>::type, type_three>::value,          "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_five>::type, type_four>::value,           "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_six>::type, type_five>::value,            "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_seven>::type, type_six>::value,           "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_eight>::type, type_seven>::value,         "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, type_nine>::type, type_eight>::value,          "not found in big map!");
+static_assert(std::is_same<brigand::lookup<big_map, bool>::type, brigand::no_such_type_>::value,   "found in big map!");
