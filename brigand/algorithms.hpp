a1f2ae5e8eb4cb0c1b5c862b0bc9184e2128178d
dodheim <dodheim@gmail.com>
2015-08-23 17:31:27 -0700

    Reverting filter addition for PR to master

--- brigand/algorithms.hpp
+++ brigand/algorithms.hpp
@@ -10,7 +10,6 @@
 #include <brigand/algorithms/any.hpp>
 #include <brigand/algorithms/apply.hpp>
 #include <brigand/algorithms/count.hpp>
-#include <brigand/algorithms/filter.hpp>
 #include <brigand/algorithms/find.hpp>
 #include <brigand/algorithms/fold.hpp>
 #include <brigand/algorithms/for_each.hpp>
