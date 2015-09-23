8f9bdff5edc21cb45b0fda1efc09f3d0e311214e
Edouard A <edouard.alligand@bureau14.fr>
2015-09-23 15:56:18 -0700

    Add support for next and prev, fix unit tests for range and make_sequence
    Added as_integral_list adapter

---
+++ brigand/adapted/integral_list.hpp
@@ -0,0 +1,20 @@
+/*==================================================================================================
+Copyright (c) 2015 Edouard Alligand and Joel Falcou
+
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+=================================================================================================**/
+#pragma once
+
+#include <brigand/algorithms/transform.hpp>
+
+namespace brigand
+{
+
+	template <typename T>
+	struct make_integral : std::integral_constant < typename T::value_type, T::value > {};
+	
+	template <typename L>
+	using as_integral_list = transform < make_integral<brigand::_1>, L > ;
+
+}
