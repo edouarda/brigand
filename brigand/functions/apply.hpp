8ae1d62ffd7c5640dc4fd604fa3a37691dc40632
Joel Falcou <joel.falcou@lri.fr>
2015-10-01 01:25:33 +0200

    Fix apply header and test

--- brigand/functions/apply.hpp
+++ brigand/functions/apply.hpp
@@ -1,12 +1,12 @@
 /*==================================================================================================
-  Copyright (c) 2015 Edouard Alligan and Joel Falcou
+  Copyright (c) 2015 Edouard Alligand and Joel Falcou
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 =================================================================================================**/
 #pragma once
 
-#include <brigand/functions/lambda.hpp>
+#include <brigand/functions/lambda/lambda.hpp>
 #include <brigand/sequences/list.hpp>
 
 namespace brigand
--- test/apply.cpp
+++ test/apply.cpp
@@ -9,3 +9,14 @@ static_assert ( brigand::apply < std::is_same<brigand::_5, brigand::_1>
                                 >::value == true
               , "invalid apply3 result"
               );
+
+struct is_int
+{
+  template<typename T> struct apply
+  {
+    using type  = typename std::is_same<T,int>::type;
+  };
+};
+
+static_assert(brigand::apply<is_int,float>::value == false, "invalid apply1 result");
+static_assert(brigand::apply<is_int,int>::value   == true, "invalid apply2 result");
