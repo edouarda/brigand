8743e70935bb7439921780cd5eeac6c12975b203
Joel Falcou <joel.falcou@lri.fr>
2015-10-01 01:26:14 +0200

    Licence typos

--- brigand/sequences/has_key.hpp
+++ brigand/sequences/has_key.hpp
@@ -1,5 +1,5 @@
 /*==================================================================================================
-  Copyright (c) 2015 Edouard Alligan and Joel Falcou
+  Copyright (c) 2015 Edouard Alligand and Joel Falcou
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
