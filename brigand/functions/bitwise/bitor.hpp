8878a01f6037d6b644c1557d7a66140ba65d5c6e
jonathan poelen <jonathan.poelen@gmail.com>
2015-10-04 00:13:03 +0200

    Fix #85 - Implements bitwise operators: bitand_, bitor_, bitxor_, shift_left and shift_right

---
+++ brigand/functions/bitwise/bitor.hpp
@@ -0,0 +1,15 @@
+/*==================================================================================================
+  Copyright (c) 2015 Edouard Alligand and Joel Falcou
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+=================================================================================================**/
+#pragma once
+
+#include <type_traits>
+
+namespace brigand
+{
+  template <typename A, typename B>
+  struct bitor_ : std::integral_constant<typename A::value_type, A::value | B::value> {};
+}
