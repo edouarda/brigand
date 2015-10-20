3b6901c8f19566f6b78939edcced47db75ccac48
Edouard A <edouard@quasardb.net>
2015-10-20 17:28:06 +0200

    Fix clang compilation issue

--- test/make_sequence_test.cpp
+++ test/make_sequence_test.cpp
@@ -5,7 +5,7 @@
 
 static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::uint32_t<0>, 3>>,
   brigand::integral_list<std::uint32_t, 0, 1, 2>>::value, "invalid make_sequence result");
-static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::uint32_t<2>, 4, brigand::prev>>,
-  brigand::integral_list<std::uint32_t, 2, 1, 0, -1>>::value, "invalid make_sequence result");
+static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::int32_t<2>, 4, brigand::prev>>,
+  brigand::integral_list<std::int32_t, 2, 1, 0, -1>>::value, "invalid make_sequence result");
 static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::uint32_t<0>, 10>>,
   brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>>::value, "invalid make_sequence result");
