8cf6ecca680f925dcdb34953e1b3ec7584b305d3
Marek Kurdej <marek@quasardb.net>
2015-12-07 18:03:56 +0100

    Fix MSVC.

--- brigand/algorithms/detail/replace.hpp
+++ brigand/algorithms/detail/replace.hpp
@@ -1,5 +1,6 @@
 /*==================================================================================================
   Copyright (c) 2015 Edouard Alligand and Joel Falcou
+  Author: Marek Kurdej
 
   Distributed under the Boost Software License, Version 1.0.
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
@@ -25,13 +26,18 @@ namespace brigand { namespace detail
     using type = Sequence;
   };
 
+  template <typename Predicate, typename T, typename NewType>
+  struct predicate_test {
+    using type = typename std::conditional<
+      /*B=*/brigand::BRIGAND_DEDUCED_TEMPLATE apply<Predicate, T>::type::value,
+      /*T=*/NewType,
+      /*F=*/T>::type;
+  };
+
   template<template<class...> class Sequence, typename Predicate, typename NewType, typename... T>
   struct replace_if_impl<Sequence<T...>, Predicate, NewType>
   {
-    using type = Sequence<typename std::conditional<
-        /*B=*/brigand::BRIGAND_DEDUCED_TEMPLATE apply<Predicate, T>::type::value,
-        /*T=*/NewType,
-        /*F=*/T>::type...>;
+    using type = Sequence<typename predicate_test<Predicate, T, NewType>::type...>;
   };
 
 } }
