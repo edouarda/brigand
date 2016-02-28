e6d1625d705fb911df3b8ee735e3dfcfcd0a223e
jonathan poelen <jonathan.poelen@gmail.com>
2016-02-28 23:34:04 +0100

    optimized filled_list

--- brigand/sequences/filled_list.hpp
+++ brigand/sequences/filled_list.hpp
@@ -7,123 +7,242 @@
 #pragma once
 
 #include <brigand/sequences/list.hpp>
+#include <brigand/sequences/append.hpp>
 
 namespace brigand
 {
 namespace detail
 {
-  template<class T, unsigned N, class List>
+  template<unsigned N, class List, bool>
   struct filled_list_impl;
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 0, List<E...>>
+  template<class T, template<class...> class, class List>
+  struct filled_list_adapted;
+
+  template<class T, class U>
+  struct type_adapt
   {
-    using type = List<E...>;
+    using type = U;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 1, List<E...>>
+  template<class T, template<class...> class List, class... Ts>
+  struct filled_list_adapted<T, List, list<Ts...>>
   {
-    using type = List<E..., T>;
+    using type = List<typename type_adapt<Ts,T>::type...>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 2, List<E...>>
+  template<template<class...> class List, class... Ts>
+  struct filled_list_adapted<int, List, list<Ts...>>
   {
-    using type = List<E..., T, T>;
+    using type = List<Ts...>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 3, List<E...>>
+  template<class T, unsigned N, template<class...> class List>
+  struct filled_list
   {
-    using type = List<E..., T, T, T>;
+    using type = typename filled_list_adapted<
+      T, List,
+      typename filled_list_impl<
+        N, list<int,int,int,int,int,int,int,int>, (8*2 <= N-8)
+      >::type
+    >::type;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 4, List<E...>>
+  template<unsigned N>
+  struct filled_list<int, N, list>
   {
-    using type = List<E..., T, T, T, T>;
+    using type = typename filled_list_impl<
+      N, list<int,int,int,int,int,int,int,int>, (8*2 <= N-8)
+    >::type;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 5, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 0, List>
   {
-    using type = List<E..., T, T, T, T, T>;
+    using type = List<>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 6, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 1, List>
   {
-    using type = List<E..., T, T, T, T, T, T>;
+    using type = List<T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 7, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 2, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T>;
+    using type = List<T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 8, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 3, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T>;
+    using type = List<T, T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 9, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 4, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T>;
+    using type = List<T, T, T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 10, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 5, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T>;
+    using type = List<T, T, T, T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 11, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 6, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T, T>;
+    using type = List<T, T, T, T, T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 12, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 7, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T, T, T>;
+    using type = List<T, T, T, T, T, T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 13, List<E...>>
+  template<class T, template<class...> class List>
+  struct filled_list<T, 8, List>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T, T, T, T>;
+    using type = List<T, T, T, T, T, T, T, T>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 14, List<E...>>
+  template<>
+  struct filled_list<int, 0, list>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T, T, T, T, T>;
+    using type = list<>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 15, List<E...>>
+  template<>
+  struct filled_list<int, 1, list>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T, T, T, T, T, T>;
+    using type = list<int>;
   };
 
-  template<class T, template<class...> class List, class... E>
-  struct filled_list_impl<T, 16, List<E...>>
+  template<>
+  struct filled_list<int, 2, list>
   {
-    using type = List<E..., T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T>;
+    using type = list<int, int>;
   };
 
-  template<class T, unsigned N, template<class...> class List, class... E>
-  struct filled_list_impl<T, N, List<E...>>
+  template<>
+  struct filled_list<int, 3, list>
   {
-    using type = typename filled_list_impl<T, N-16, List<E..., T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T>>::type;
+    using type = list<int, int, int>;
+  };
+
+  template<>
+  struct filled_list<int, 4, list>
+  {
+    using type = list<int, int, int, int>;
+  };
+
+  template<>
+  struct filled_list<int, 5, list>
+  {
+    using type = list<int, int, int, int, int>;
+  };
+
+  template<>
+  struct filled_list<int, 6, list>
+  {
+    using type = list<int, int, int, int, int, int>;
+  };
+
+  template<>
+  struct filled_list<int, 7, list>
+  {
+    using type = list<int, int, int, int, int, int, int>;
+  };
+
+  template<>
+  struct filled_list<int, 8, list>
+  {
+    using type = list<int, int, int, int, int, int, int, int>;
+  };
+
+
+  template<class... E>
+  struct filled_list_impl<0, list<E...>, false>
+  {
+    using type = list<E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<1, list<E...>, false>
+  {
+    using type = list<int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<2, list<E...>, false>
+  {
+    using type = list<int, int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<3, list<E...>, false>
+  {
+    using type = list<int, int, int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<4, list<E...>, false>
+  {
+    using type = list<int, int, int, int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<5, list<E...>, false>
+  {
+    using type = list<int, int, int, int, int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<6, list<E...>, false>
+  {
+    using type = list<int, int, int, int, int, int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<7, list<E...>, false>
+  {
+    using type = list<int, int, int, int, int, int, int, E...>;
+  };
+
+  template<class... E>
+  struct filled_list_impl<8, list<E...>, false>
+  {
+    using type = list<int, int, int, int, int, int, int, int, E...>;
+  };
+
+  template<unsigned N, class... E>
+  struct filled_list_impl<N, list<E...>, false>
+  {
+    using type = typename append_impl<
+      list<E...>,
+      typename filled_list_impl<
+        N-8,
+        list<int, int, int, int, int, int, int, int>,
+        (8*2 <= N-8)
+      >::type
+    >::type;
+  };
+
+  template<unsigned N, class... E>
+  struct filled_list_impl<N, list<E...>, true>
+  {
+    using type = typename filled_list_impl<
+      N-sizeof...(E),
+      list<E..., E...>,
+      (sizeof...(E)*2 <= N-sizeof...(E))
+    >::type;
   };
 }
 
   template<class T, unsigned N, template<class...> class List = list>
-  using filled_list = typename detail::filled_list_impl<T, N, List<>>::type;
+  using filled_list = typename detail::filled_list<T, N, List>::type;
 }
