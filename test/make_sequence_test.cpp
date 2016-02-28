fe93cb3168148631f213ae6b522da90d19814820
jonathan poelen <jonathan.poelen@gmail.com>
2016-02-28 05:49:06 +0100

    as_integral_list is not necessary

--- test/make_sequence_test.cpp
+++ test/make_sequence_test.cpp
@@ -1,13 +1,12 @@
 #include <brigand/sequences/make_sequence.hpp>
 #include <brigand/functions/arithmetic/prev.hpp>
 #include <brigand/types/integer.hpp>
-#include <brigand/adapted/integral_list.hpp>
 
-static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::uint32_t<0>, 3>>,
+static_assert(std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 3>,
   brigand::integral_list<std::uint32_t, 0, 1, 2>>::value, "invalid make_sequence result");
-static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::int32_t<2>, 4, brigand::quote<brigand::prev>>>,
+static_assert(std::is_same<brigand::make_sequence<brigand::int32_t<2>, 4, brigand::quote<brigand::prev>>,
   brigand::integral_list<std::int32_t, 2, 1, 0, -1>>::value, "invalid make_sequence result");
-static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::uint32_t<0>, 10>>,
+static_assert(std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 10>,
   brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>>::value, "invalid make_sequence result");
-static_assert(std::is_same<brigand::as_integral_list<brigand::make_sequence<brigand::uint32_t<0>, 19>>,
+static_assert(std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 19>,
   brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18>>::value, "invalid make_sequence result");
