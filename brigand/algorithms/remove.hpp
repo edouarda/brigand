b3bf5714508e3fe463e6c954e6c220e14b2c8a46
Odin Holmes <holmes.odin@gmail.com>
2016-03-02 20:08:35 +0100

    optimized brigand::remove

--- brigand/algorithms/remove.hpp
+++ brigand/algorithms/remove.hpp
@@ -6,51 +6,50 @@
 =================================================================================================**/
 #pragma once
 
-#include <brigand/sequences/clear.hpp>
 #include <brigand/functions/lambda/apply.hpp>
+#include <brigand/sequences/clear.hpp>
 #include <type_traits>
 
 namespace brigand
 {
-  namespace detail
-  {
-    template< class L1, typename Pred, class L2 = clear<L1>>
+namespace detail
+{
+    template <class L1, typename Pred, class L2 = clear<L1>>
     struct remove_if_impl
     {
-      using type = L2;
+        using type = L2;
     };
 
-    template< class L1, typename Pred, class L2, bool >
+    template <class L1, typename Pred, class L2, bool>
     struct remove_if_shortcut;
 
-    template< template<class...> class L1, class T, class... Ts,
-              typename Pred,
-              template<class...> class L2, class... Us >
+    template <template <class...> class L1, class T, class... Ts, typename Pred,
+              template <class...> class L2, class... Us>
     struct remove_if_shortcut<L1<T, Ts...>, Pred, L2<Us...>, true>
-    : remove_if_impl<L1<Ts...>, Pred, L2<Us...>>
-    {};
+        : remove_if_impl<L1<Ts...>, Pred, L2<Us...>>
+    {
+    };
 
-    template< template<class...> class L1, class T, class... Ts,
-              typename Pred,
-              template<class...> class L2, class... Us >
+    template <template <class...> class L1, class T, class... Ts, typename Pred,
+              template <class...> class L2, class... Us>
     struct remove_if_shortcut<L1<T, Ts...>, Pred, L2<Us...>, false>
-    : remove_if_impl<L1<Ts...>, Pred, L2<Us..., T>>
-    {};
+        : remove_if_impl<L1<Ts...>, Pred, L2<Us..., T>>
+    {
+    };
 
-    template<typename Pred, typename T> using pred = brigand::apply<Pred,T>;
+    template <typename Pred, typename T>
+    using pred = brigand::apply<Pred, T>;
 
-    template< template<class...> class L1, class T, class... Ts,
-              typename Pred, class L2 >
+    template <template <class...> class L1, class T, class... Ts, typename Pred, class L2>
     struct remove_if_impl<L1<T, Ts...>, Pred, L2>
-    : remove_if_shortcut< L1<T, Ts...>, Pred, L2
-                        , bool(pred<Pred,T>::value)
-                        >
-    {};
-  }
+        : remove_if_shortcut<L1<T, Ts...>, Pred, L2, bool(pred<Pred, T>::value)>
+    {
+    };
+}
 
 namespace lazy
 {
-    template<typename L, typename Pred>
+    template <typename L, typename Pred>
     using remove_if = typename detail::remove_if_impl<L, Pred>;
 }
 
@@ -59,32 +58,23 @@ using remove_if = typename ::brigand::lazy::remove_if<L, Pred>::type;
 
 namespace detail
 {
-    template< typename L1, typename T, typename L2 = clear<L1>>
-    struct remove_element_impl
+    template <typename T>
+    struct remove_if_pred
     {
-      using type = L2;
+        template <typename U>
+        struct apply
+        {
+            using type =
+                typename std::conditional<std::is_same<T, U>::value, list<>, list<U>>::type;
+        };
     };
-
-    template< template<class...> class L1, typename T, typename... Ts,
-              typename U, template<class...> class L2, typename... Us >
-    struct remove_element_impl<L1<T, Ts...>, U, L2<Us...>>
-    : remove_element_impl<L1<Ts...>, U, L2<Us..., T>>
-    {};
-
-    template< template<class...> class L1, typename T, typename... Ts,
-              template<class...> class L2, typename... Us >
-    struct remove_element_impl<L1<T, Ts...>, T, L2<Us...>>
-    : remove_element_impl<L1<Ts...>, T, L2<Us...>>
-    {};
 }
-
 namespace lazy
 {
     template <typename L, typename T>
-    using remove = typename detail::remove_element_impl<L, T>;
+    using remove = brigand::wrap<brigand::transform<L, detail::remove_if_pred<T>>, brigand::append>;
 }
 
 template <typename L, typename T>
-using remove = typename ::brigand::lazy::remove<L, T>::type;
-
+using remove = lazy::remove<L, T>;
 }
