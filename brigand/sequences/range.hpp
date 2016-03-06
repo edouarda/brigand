2ca5a2acf72ed6f2775d6b0d01499685b93e5fa4
jonathan poelen <jonathan.poelen@gmail.com>
2016-03-06 05:15:17 +0100

    fix reverse_range with msvc

--- brigand/sequences/range.hpp
+++ brigand/sequences/range.hpp
@@ -51,30 +51,48 @@ namespace brigand
       using type = list<>;
     };
 
-    template<class T, T Start, class L, std::size_t N>
-    struct reverse_range_adapt;
+
+    template<class T, class, class, T>
+    struct reverse_range_cat;
 
     template<class T, T Start, T Int>
     struct int_minus
     {
-      using type = std::integral_constant<T, Start - Int>;
+      using type = std::integral_constant<T, Int - Start>;
     };
 
-    template<class T, T Start, std::size_t... Ints, std::size_t N>
-    struct reverse_range_adapt<T, Start, list<std::integral_constant<T, Ints>...>, N>
+    template<class T, class... Ts, T... Ints, T Start>
+    struct reverse_range_cat<T, list<Ts...>, list<std::integral_constant<T, Ints>...>, Start>
     {
-      using type = list<typename int_minus<T, N + Start, Ints>::type...>;
+      using type = list<Ts..., typename int_minus<T, Start, Ints>::type...>;
     };
 
     template<class T, T Start, std::size_t N>
     struct reverse_range_impl
-    : reverse_range_adapt<T, Start, typename range_impl<T, 0, N>::type, N>
+    : reverse_range_cat<
+      T,
+      typename reverse_range_impl<T, Start, N/2>::type,
+      typename reverse_range_impl<T, Start, N - N/2>::type,
+      N/2
+    >
     {};
+
+    template<class T, T Start>
+    struct reverse_range_impl<T, Start, 1>
+    {
+      using type = list<std::integral_constant<T, Start>>;
+    };
+
+    template<class T, T Start>
+    struct reverse_range_impl<T, Start, 0>
+    {
+      using type = list<>;
+    };
   }
 
   template<class T, T Start, T Stop>
   using range = typename detail::range_impl<T, Start, Stop-Start>::type;
 
   template<class T, T Start, T Stop>
-  using reverse_range = typename detail::reverse_range_impl<T, Stop, Start-Stop>::type;
+  using reverse_range = typename detail::reverse_range_impl<T, Start, Start-Stop>::type;
 }
