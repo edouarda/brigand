8a9b9a0619e064712cb13a0095e87da033fa0b8e
jonathan poelen <jonathan.poelen@gmail.com>
2016-03-06 22:42:47 +0100

    implementation moved inside of brigand::detail

--- brigand/sequences/front.hpp
+++ brigand/sequences/front.hpp
@@ -9,50 +9,47 @@
 namespace brigand
 {
   // push_front
-  template<class L, class... T> struct push_front_impl;
-
-  template<template<class...> class L, class... U, class... T>
-  struct push_front_impl<L<U...>, T...>
+  namespace detail
   {
+    template<class L, class... T> struct push_front_impl;
+
+    template<template<class...> class L, class... U, class... T>
+    struct push_front_impl<L<U...>, T...>
+    {
       using type = L<T..., U...>;
-  };
+    };
+  }
 
   template<class L, class... T>
-  using push_front = typename push_front_impl<L, T...>::type;
+  using push_front = typename detail::push_front_impl<L, T...>::type;
 
   // front
-  template <class First, class...>
-  struct first_element
+  namespace detail
   {
-      using type = First;
-  };
+    template<class L> struct front_impl;
 
-  template<class L> struct front_impl;
-
-  template<template<class...> class L, class... U>
-  struct front_impl<L<U...>>
-  {
-      using type = typename first_element<U...>::type;
-  };
+    template<template<class...> class L, class T, class... U>
+    struct front_impl<L<T, U...>>
+    {
+      using type = T;
+    };
+  }
 
   template <class L>
-  using front = typename front_impl<L>::type;
+  using front = typename detail::front_impl<L>::type;
 
   // pop front
-  template <template <class...> class L, class First, class... R>
-  struct without_first_element
+  namespace detail
   {
-      using type = L<R...>;
-  };
+    template <class L> struct pop_front_impl;
 
-  template <class L> struct pop_front_impl;
-
-  template<template<class...> class L, class... U>
-  struct pop_front_impl<L<U...>>
-  {
-      using type = typename without_first_element<L, U...>::type;
-  };
+    template<template<class...> class L, class T, class... U>
+    struct pop_front_impl<L<T, U...>>
+    {
+      using type = L<U...>;
+    };
+  }
 
   template <class L>
-  using pop_front = typename pop_front_impl<L>::type;
+  using pop_front = typename detail::pop_front_impl<L>::type;
 }
