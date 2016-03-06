019750066792f2b46b10d6aaf2ba900710313261
jonathan poelen <jonathan.poelen@gmail.com>
2016-03-06 03:40:36 +0100

    fast tracked brigand::range (thanks @brunocodutra /metal/number/enumerate.hpp)

--- brigand/sequences/range.hpp
+++ brigand/sequences/range.hpp
@@ -5,14 +5,64 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 =================================================================================================**/
 #pragma once
-#include <brigand/sequences/make_sequence.hpp>
-#include <brigand/functions/arithmetic/prev.hpp>
+
+#include <type_traits>
+
+#include <brigand/sequences/list.hpp>
 
 namespace brigand
 {
-    template<class T, T Start, T Stop>
-    using range = make_sequence<std::integral_constant<T, Start>, Stop - Start>;
+  namespace detail
+  {
+    template<class T, class, class, T>
+    struct range_cat;
 
-    template<class T, T Start, T Stop>
-    using reverse_range = make_sequence<std::integral_constant<T, Start>, Start - Stop, quote<prev>>;
+    template<class T, class... Ts, T... Ints, T Start>
+    struct range_cat<T, list<Ts...>, list<std::integral_constant<T, Ints>...>, Start>
+    {
+      using type = list<Ts..., std::integral_constant<T, Start + Ints>...>;
+    };
+
+    template<class T, T Start, std::size_t N>
+    struct range_impl
+    : range_cat<
+      T,
+      typename range_impl<T, Start, N/2>::type,
+      typename range_impl<T, Start, N - N/2>::type,
+      N/2
+    >
+    {};
+
+    template<class T, T Start>
+    struct range_impl<T, Start, 1>
+    {
+      using type = list<std::integral_constant<T, Start>>;
+    };
+
+    template<class T, T Start>
+    struct range_impl<T, Start, 0>
+    {
+      using type = list<>;
+    };
+
+    template<class T, T Start, class L, std::size_t N>
+    struct reverse_range_adapt;
+
+    template<class T, T Start, std::size_t... Ints, std::size_t N>
+    struct reverse_range_adapt<T, Start, list<std::integral_constant<T, Ints>...>, N>
+    {
+      using type = list<std::integral_constant<T, T(N - Ints + Start)>...>;
+    };
+
+    template<class T, T Start, std::size_t N>
+    struct reverse_range_impl
+    : reverse_range_adapt<T, Start, typename range_impl<T, 0, N>::type, N>
+    {};
+  }
+
+  template<class T, T Start, T Stop>
+  using range = typename detail::range_impl<T, Start, Stop-Start>::type;
+
+  template<class T, T Start, T Stop>
+  using reverse_range = typename detail::reverse_range_impl<T, Stop, Start-Stop>::type;
 }
