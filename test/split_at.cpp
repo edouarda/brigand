2388749b98c23e0a1fc9dddaaec87a4096a5ea72
Odin Holmes <holmes.odin@gmail.com>
2016-03-10 14:13:20 +0100

    added custom list tests

--- test/split_at.cpp
+++ test/split_at.cpp
@@ -53,3 +53,61 @@ static_assert(std::is_same<brigand::split_at<fast_track_list, std::integral_cons
                                                        bool, int, void, char, char, char, bool>,
                                          brigand::list<int, void, int>>>::value,
               "invalid split on sequence with no delim");
+
+namespace custom
+{
+template <class...>
+class custom_list
+{
+};
+static_assert(std::is_same<brigand::split_at<custom_list<>, std::integral_constant<int, 0>>,
+                           custom_list<custom_list<>, custom_list<>>>::value,
+              "invalid split on empty sequence");
+
+using small_seq = custom_list<char, bool, int>;
+
+static_assert(std::is_same<brigand::split_at<small_seq, std::integral_constant<int, 0>>,
+                           custom_list<custom_list<>, small_seq>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<small_seq, std::integral_constant<int, 3>>,
+                           custom_list<small_seq, custom_list<>>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<small_seq, std::integral_constant<int, 2>>,
+                           custom_list<custom_list<char, bool>, custom_list<int>>>::value,
+              "invalid split on sequence with no delim");
+
+using no_fast_track_list = custom_list<char, bool, int, void, char, char, char, bool, int, void,
+                                       char, char, char, bool, int, void>;
+using fast_track_list = custom_list<char, bool, int, void, char, char, char, bool, int, void, char,
+                                    char, char, bool, int, void, int>;
+
+static_assert(std::is_same<brigand::split_at<no_fast_track_list, std::integral_constant<int, 0>>,
+                           custom_list<custom_list<>, no_fast_track_list>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<no_fast_track_list, std::integral_constant<int, 16>>,
+                           custom_list<no_fast_track_list, custom_list<>>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<no_fast_track_list, std::integral_constant<int, 14>>,
+                           custom_list<custom_list<char, bool, int, void, char, char, char, bool,
+                                                   int, void, char, char, char, bool>,
+                                       custom_list<int, void>>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<fast_track_list, std::integral_constant<int, 0>>,
+                           custom_list<custom_list<>, fast_track_list>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<fast_track_list, std::integral_constant<int, 17>>,
+                           custom_list<fast_track_list, custom_list<>>>::value,
+              "invalid split on sequence with no delim");
+
+static_assert(std::is_same<brigand::split_at<fast_track_list, std::integral_constant<int, 14>>,
+                           custom_list<custom_list<char, bool, int, void, char, char, char, bool,
+                                                   int, void, char, char, char, bool>,
+                                       custom_list<int, void, int>>>::value,
+              "invalid split on sequence with no delim");
+}
