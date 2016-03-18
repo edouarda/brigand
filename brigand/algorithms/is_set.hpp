4c4a7e081263f73cd244c6bd2c734cf74dbbd4ed
jonathan poelen <jonathan.poelen@gmail.com>
2016-03-18 22:45:21 +0100

    removed useless include

--- brigand/algorithms/is_set.hpp
+++ brigand/algorithms/is_set.hpp
@@ -6,8 +6,6 @@
 =================================================================================================**/
 #pragma once
 
-#include <initializer_list>
-
 #include <brigand/sequences/range.hpp>
 #include <brigand/types/type.hpp>
 
