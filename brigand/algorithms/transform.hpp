e7bee3154ea7e3582282b347fe60c7e8d7f6f6cd
jonathan poelen <jonathan.poelen@gmail.com>
2016-06-17 02:07:35 +0200

    simplified transform

--- brigand/algorithms/transform.hpp
+++ brigand/algorithms/transform.hpp
@@ -12,58 +12,27 @@ namespace brigand
 {
   namespace detail
   {
+    template<std::size_t, class Seq1, class FuncOrSeq2, class FuncOrvoid = void>
+    struct transform_selector;
 
-  template <typename Functor, typename Sequence>
-  struct unary_transform_impl;
-
-    // Unary transform implementation
-    template< typename Functor
-            , template<class...> class Sequence, typename... T
-            >
-  struct unary_transform_impl<Functor, Sequence<T...>>
+    template<template<class...> class Seq, class... T, class Func>
+    struct transform_selector<0, Seq<T...>, Func, void>
     {
-      using type = Sequence< brigand::apply<Functor,T>... >;
+      using type = Seq<brigand::apply<Func, T>...>;
     };
 
-  template<typename Functor, typename Sequence1, typename Sequence2>
-  struct binary_transform_impl;
-
-    // Binary transform implementation
-    template< typename Functor
-            , template<class...> class Sequence1, typename... T1
-            , template<class...> class Sequence2, typename... T2
-            >
-  struct binary_transform_impl<Functor, Sequence1<T1...>, Sequence2<T2...>>
+    template<template<class...> class Seq1, class... T1, template<class...> class Seq2, class... T2, class Func>
+    struct transform_selector<1, Seq1<T1...>, Seq2<T2...>, Func>
     {
-      static_assert ( sizeof...(T1) == sizeof...(T2)
-                    , "The arguments of transform should be of the same size"
-                    );
-
-      using type = Sequence1< brigand::apply<Functor,T1,T2>... >;
-  };
-
-  template <std::size_t Selector, typename Sequence1, typename Functor, typename Sequence2 = void>
-  struct transform_selector;
-
-  template <typename Sequence1, typename Functor>
-  struct transform_selector <0, Sequence1, Functor, void>
-  {
-    using type = typename unary_transform_impl<Functor, Sequence1>::type;
-  };
-
-  template <typename Sequence1, typename Sequence2, typename Functor>
-  struct transform_selector<1, Sequence1, Sequence2, Functor>
-  {
-    using type = typename binary_transform_impl<Functor, Sequence1, Sequence2>::type;
-  };
-
+      using type = Seq1<brigand::apply<Func, T1, T2>...>;
+    };
   }
 
-namespace lazy
-{
+  namespace lazy
+  {
     template<typename Sequence1, typename OpSeq1, typename... OpSeq2>
     using transform = typename detail::transform_selector<sizeof...(OpSeq2), Sequence1, OpSeq1, OpSeq2...>;
-}
+  }
 
   // Main transform entry point
   template<typename Sequence1, typename OpSeq1, typename... OpSeq2>
