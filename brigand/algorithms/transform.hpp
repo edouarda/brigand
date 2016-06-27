d9905fa2493576d37628c11c47874c401ac909a6
jonathan poelen <jonathan.poelen@gmail.com>
2016-06-27 03:28:56 +0200

    transform with any number of sequence

--- brigand/algorithms/transform.hpp
+++ brigand/algorithms/transform.hpp
@@ -7,16 +7,77 @@
 #pragma once
 
 #include <brigand/functions/lambda/apply.hpp>
+#include <brigand/sequences/back.hpp>
+#include <brigand/sequences/filled_list.hpp>
+#include <brigand/sequences/size.hpp>
 
 namespace brigand
 {
   namespace detail
   {
-    template<std::size_t, class Seq1, class FuncOrSeq2, class FuncOrvoid = void>
-    struct transform_selector;
+    template<class L1, class L2>
+    struct rot90;
+
+    template<
+      class... L1,
+      template<class...> class S1, class... T1,
+      template<class...> class S2, class... T2,
+      template<class...> class S3, class... T3,
+      template<class...> class S4, class... T4,
+      template<class...> class S5, class... T5,
+      template<class...> class S6, class... T6,
+      template<class...> class S7, class... T7,
+      template<class...> class S8, class... T8,
+      class... L2>
+    struct rot90<list<L1...>, list<
+      S1<T1...>, S2<T2...>, S3<T3...>, S4<T4...>,
+      S5<T5...>, S6<T6...>, S7<T7...>, S8<T8...>, L2...>>
+    : rot90<list<push_back<L1, T1, T2, T3, T4, T5, T6, T7, T8>...>, list<L2...>>
+    {};
+
+    template<class... L1, template<class...> class S, class... T, class... L2>
+    struct rot90<list<L1...>, list<S<T...>, L2...>>
+    : rot90<list<push_back<L1, T>...>, list<L2...>>
+    {};
+
+    template<class L1>
+    struct rot90<L1, list<>>
+    {
+      using type = L1;
+    };
+
+
+    template<class Func, class Seq1, class Seq2, class Seqs>
+    struct transform_impl;
+
+    template<class F, class T1, class T2, class Seq>
+    struct transform_apply;
+
+    template<class F, class T1, class T2, class... Ts>
+    struct transform_apply<F, T1, T2, list<Ts...>>
+    {
+      using type = brigand::apply<F, T1, T2, Ts...>;
+    };
+
+    template<
+      class Func,
+      template<class...> class Seq1, class... T1,
+      template<class...> class Seq2, class... T2,
+      class... Seqs>
+    struct transform_impl<Func, Seq1<T1...>, Seq2<T2...>, list<Seqs...>>
+    {
+      using type = Seq1<typename transform_apply<Func, T1, T2, Seqs>::type...>;
+    };
+
+    template<std::size_t n, class Seq1, class Seq2, class... FuncOrSeqs>
+    struct transform_selector
+    : transform_impl<at_c<list<FuncOrSeqs...>, n-1>, Seq1, Seq2,
+      typename rot90<filled_list<list<>, size<Seq1>::value>, pop_back<list<FuncOrSeqs...>>>::type>
+    {};
+
 
     template<template<class...> class Seq, class... T, class Func>
-    struct transform_selector<0, Seq<T...>, Func, void>
+    struct transform_selector<0, Seq<T...>, Func>
     {
       using type = Seq<brigand::apply<Func, T>...>;
     };
