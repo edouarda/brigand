354ac91625103d8686906726b783e969e34b20da
Odin Holmes <holmes.odin@gmail.com>
2016-06-28 17:39:25 +0200

    fixed apply test

--- test/apply.cpp
+++ test/apply.cpp
@@ -10,13 +10,11 @@ static_assert ( brigand::apply < std::is_same<brigand::_5, brigand::_1>
               , "invalid apply3 result"
               );
 
+template<typename T>
 struct is_int
 {
-  template<typename T> struct apply
-  {
-    using type  = typename std::is_same<T,int>::type;
-  };
+	using type = typename std::is_same<T, int>::type;
 };
 
-static_assert(brigand::apply<is_int,float>::value == false, "invalid apply1 result");
-static_assert(brigand::apply<is_int,int>::value   == true, "invalid apply2 result");
+static_assert(brigand::apply<is_int<_1>, float>::value == false, "invalid apply1 result");
+static_assert(brigand::apply<is_int<_1>, int>::value == true, "invalid apply2 result");
