895a4d56c439846a54ae42a68216ae5e1c491b7b
Odin Holmes <holmes.odin@gmail.com>
2016-07-04 14:53:10 +0200

    updated tests

--- test/make_sequence_test.cpp
+++ test/make_sequence_test.cpp
@@ -1,12 +1,19 @@
-#include <brigand/sequences/make_sequence.hpp>
 #include <brigand/functions/arithmetic/prev.hpp>
+#include <brigand/sequences/make_sequence.hpp>
 #include <brigand/types/integer.hpp>
 
 static_assert(std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 3>,
-  brigand::integral_list<std::uint32_t, 0, 1, 2>>::value, "invalid make_sequence result");
-static_assert(std::is_same<brigand::make_sequence<brigand::int32_t<2>, 4, brigand::quote<brigand::prev>>,
-  brigand::integral_list<std::int32_t, 2, 1, 0, -1>>::value, "invalid make_sequence result");
-static_assert(std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 10>,
-  brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>>::value, "invalid make_sequence result");
+                           brigand::integral_list<std::uint32_t, 0, 1, 2>>::value,
+              "invalid make_sequence result");
+static_assert(
+    std::is_same<brigand::make_sequence<brigand::int32_t<2>, 4, brigand::prev<brigand::_1>>,
+                 brigand::integral_list<std::int32_t, 2, 1, 0, -1>>::value,
+    "invalid make_sequence result");
+static_assert(
+    std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 10>,
+                 brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9>>::value,
+    "invalid make_sequence result");
 static_assert(std::is_same<brigand::make_sequence<brigand::uint32_t<0>, 19>,
-  brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18>>::value, "invalid make_sequence result");
+                           brigand::integral_list<std::uint32_t, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                                  11, 12, 13, 14, 15, 16, 17, 18>>::value,
+              "invalid make_sequence result");
--- test/replace.cpp
+++ test/replace.cpp
@@ -5,56 +5,52 @@
 brigand::replace<brigand::list<>, int, float> replace_on_empty_list = brigand::list<>{};
 
 brigand::replace<brigand::list<double, float>, int, float> replace_no_op =
-    brigand::list<double, float>{};
+brigand::list<double, float>{};
 
 brigand::replace<brigand::list<int>, int, float> replace_test1 = brigand::list<float>{};
 
 brigand::replace<brigand::list<double>, int, float> replace_test2 = brigand::list<double>{};
 
 brigand::replace<brigand::list<int, float>, int, float> replace_test3 =
-    brigand::list<float, float>{};
+brigand::list<float, float>{};
 
 brigand::replace<brigand::list<int, float, char, double, int, unsigned int>, int, float>
-    replace_test4 = brigand::list<float, float, char, double, float, unsigned int>{};
+replace_test4 = brigand::list<float, float, char, double, float, unsigned int>{};
 
-using pred_int = brigand::quote<std::is_integral>;
+using pred_int = std::is_integral<brigand::_1>;
 
 brigand::replace_if<brigand::list<int, float, char, double, int, unsigned int>, pred_int, float>
-    replace_test5 = brigand::list<float, float, float, double, float, float>{};
+replace_test5 = brigand::list<float, float, float, double, float, float>{};
 
-using pred_fp = brigand::quote<std::is_floating_point>;
+using pred_fp = std::is_floating_point<brigand::_1>;
 
 brigand::replace_if<brigand::list<int, float, char, double, int, unsigned int>, pred_fp, long>
-    replace_test6 = brigand::list<int, long, char, long, int, unsigned int>{};
+replace_test6 = brigand::list<int, long, char, long, int, unsigned int>{};
 
 namespace custom
 {
-template <class...>
-class custom_list
-{
-};
+	template <class...>
+	class custom_list
+	{
+	};
 
-brigand::replace<custom_list<>, int, float> replace_on_empty_list = custom_list<>{};
+	brigand::replace<custom_list<>, int, float> replace_on_empty_list = custom_list<>{};
 
-brigand::replace<custom_list<double, float>, int, float> replace_no_op =
-    custom_list<double, float>{};
+	brigand::replace<custom_list<double, float>, int, float> replace_no_op =
+		custom_list<double, float>{};
 
-brigand::replace<custom_list<int>, int, float> replace_test1 = custom_list<float>{};
+	brigand::replace<custom_list<int>, int, float> replace_test1 = custom_list<float>{};
 
-brigand::replace<custom_list<double>, int, float> replace_test2 = custom_list<double>{};
+	brigand::replace<custom_list<double>, int, float> replace_test2 = custom_list<double>{};
 
-brigand::replace<custom_list<int, float>, int, float> replace_test3 = custom_list<float, float>{};
+	brigand::replace<custom_list<int, float>, int, float> replace_test3 = custom_list<float, float>{};
 
-brigand::replace<custom_list<int, float, char, double, int, unsigned int>, int, float>
-    replace_test4 = custom_list<float, float, char, double, float, unsigned int>{};
+	brigand::replace<custom_list<int, float, char, double, int, unsigned int>, int, float>
+		replace_test4 = custom_list<float, float, char, double, float, unsigned int>{};
 
-using pred_int = brigand::quote<std::is_integral>;
+	brigand::replace_if<custom_list<int, float, char, double, int, unsigned int>, pred_int, float>
+		replace_test5 = custom_list<float, float, float, double, float, float>{};
 
-brigand::replace_if<custom_list<int, float, char, double, int, unsigned int>, pred_int, float>
-    replace_test5 = custom_list<float, float, float, double, float, float>{};
-
-using pred_fp = brigand::quote<std::is_floating_point>;
-
-brigand::replace_if<custom_list<int, float, char, double, int, unsigned int>, pred_fp, long>
-    replace_test6 = custom_list<int, long, char, long, int, unsigned int>{};
+	brigand::replace_if<custom_list<int, float, char, double, int, unsigned int>, pred_fp, long>
+		replace_test6 = custom_list<int, long, char, long, int, unsigned int>{};
 }
