8a86dbe0c464c75cf4f98852082afbc4281566c7
Odin Holmes <holmes.odin@gmail.com>
2016-08-09 14:06:10 +0200

    lambda fast tracked find

--- brigand/algorithms/detail/find.hpp
+++ brigand/algorithms/detail/find.hpp
@@ -13,70 +13,55 @@ namespace brigand
 {
 namespace detail
 {
-    template <typename P, template <class...> class Sequence>
+
+    template <template <typename...> class S, template <typename...> class F, typename... Ts>
     struct finder
     {
         template <typename T>
-        struct Pred : brigand::apply<P, T>
-        {
-        };
+        using P = F<Ts..., T>;
 
-        // default only if not found
-        template <bool C, bool F, typename... Ts>
+        template <bool InNext8, bool Match, typename... Ls>
         struct find
         {
-            using type = Sequence<>;
+            using type = S<>; // not found case because none left
         };
 
-        // found it, terminate
-        template <bool F, typename T1, typename... Ts>
-        struct find<true, F, T1, Ts...>
+        template <typename L>
+        struct find<true, false, L>
         {
-            using type = Sequence<T1, Ts...>;
+            using type = S<>; // not found case because only one left and not true
         };
 
-        // not found but close enough to no fast track
-        template <typename T1, typename T2, typename... Ts>
-        struct find<false, true, T1, T2, Ts...> : find<Pred<T2>::value, true, T2, Ts...>
+        template <typename L, typename... Ls> // match case
+        struct find<true, true, L, Ls...>
         {
+            using type = S<L, Ls...>;
         };
 
-        // not found and not enough to no fast track again
-        template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6,
-                  typename T7, typename T8, typename T9, typename... Ts>
-        struct find<false, false, T1, T2, T3, T4, T5, T6, T7, T8, T9, Ts...>
-            : find<
-                  Pred<T9>::value,
-                  true /*technically we don't know if its going to be in the next 8 but if not the default will kick in so its ok*/,
-                  T9, Ts...>
+        template <typename L1, typename L2, typename... Ls> // not match case
+        struct find<true, false, L1, L2, Ls...> : find<true, F<Ts..., L2>::value, L2, Ls...>
         {
         };
 
-        // not found and can fast track again
-        template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6,
-                  typename T7, typename T8, typename T9, typename T10, typename T11, typename T12,
-                  typename T13, typename T14, typename T15, typename T16, typename... Ts>
-        struct find<false, false, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
-                    T16, Ts...>
-            : find<Pred<T9>::value,
-                   (Pred<T9>::value || Pred<T10>::value || Pred<T11>::value || Pred<T12>::value ||
-                    Pred<T13>::value || Pred<T14>::value || Pred<T15>::value || Pred<T16>::value),
-                   T9, T10, T11, T12, T13, T14, T15, T16, Ts...>
+        template <typename L0, typename L1, typename L2, typename L3, typename L4, typename L5,
+                  typename L6, typename L7, typename L8,
+                  typename... Ls> // not match no longer fast track case
+        struct find<false, false, L0, L1, L2, L3, L4, L5, L6, L7, L8, Ls...>
+            : find<true, F<Ts..., L8>::value, L8, Ls...>
         {
         };
-    };
-    // By default, consider we found nothing
-    template <typename Predicate, class Sequence>
-    struct find_if_impl
-    {
-        using type = Sequence;
-    };
 
-    template <typename Predicate, template <class...> class Sequence, typename T1, typename... T>
-    struct find_if_impl<Predicate, Sequence<T1, T...>>
-        : finder<Predicate, Sequence>::template find<false, false, void, void, void, void, void,
-                                                     void, void, void, T1, T...>
-    {
+        template <typename L1, typename L2, typename L3, typename L4, typename L5, typename L6,
+                  typename L7, typename L8, typename L9, typename L10, typename L11, typename L12,
+                  typename L13, typename L14, typename L15, typename L16,
+                  typename... Ls> // not match can fast track again case
+        struct find<false, false, L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13, L14, L15,
+                    L16, Ls...>
+            : find<(P<L9>::value || P<L10>::value || P<L11>::value || P<L12>::value ||
+                    P<L13>::value || P<L14>::value || P<L15>::value || P<L16>::value),
+                   P<L9>::value, L9, L10, L11, L12, L13, L14, L15, L16, Ls...>
+        {
+        };
     };
 }
 }
