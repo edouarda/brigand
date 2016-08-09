4af5036f00b6a5e8a95ae8275fa974fc4e44a227
Odin Holmes <holmes.odin@gmail.com>
2016-08-09 15:46:27 +0200

    damn you MSVC and your not expanding multiple packs correctly

--- brigand/algorithms/replace.hpp
+++ brigand/algorithms/replace.hpp
@@ -12,11 +12,36 @@
 
 namespace brigand
 {
+
+#ifdef BRIGAND_COMP_MSVC
+	namespace detail
+	{
+		template <typename T, typename Pred, typename NewType>
+		struct replacer : std::conditional<apply<Pred, T>::value, NewType, T>
+		{
+		};
+		template <typename T, template <typename...> class F, typename NewType>
+		struct replacer<T, bind<F, _1>, NewType> : std::conditional<F<T>::value, NewType, T>
+		{
+		};
+		template <typename T, template <typename...> class F, typename NewType>
+		struct replacer<T, F<_1>, NewType> : std::conditional<F<T>::type::value, NewType, T>
+		{
+		};
+	}
+#endif
 namespace lazy
 {
 
     template <typename Sequence, typename Predicate, typename NewType>
     struct replace_if;
+#ifdef BRIGAND_COMP_MSVC
+	template <template <typename...> class S, typename... Ts, typename Predicate, typename NewType>
+	struct replace_if<S<Ts...>, Predicate, NewType>
+	{
+		using type = S<typename detail::replacer<Ts, Predicate, NewType>::type...>;
+	};
+#else
     template <template <typename...> class S, typename... Ts, typename Predicate, typename NewType>
     struct replace_if<S<Ts...>, Predicate, NewType>
     {
@@ -35,6 +60,7 @@ namespace lazy
     {
         using type = S<typename std::conditional<F<Ts>::type::value, NewType, Ts>::type...>;
     };
+#endif
 
     template <typename Sequence, typename OldType, typename NewType>
     using replace = replace_if<Sequence, std::is_same<_1, pin<OldType>>, NewType>;
