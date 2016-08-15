8ac3bba0ee150ddebf6a2c1144629015ecdd2f1c
Odin Holmes <holmes.odin@gmail.com>
2016-08-15 18:14:37 +0200

    missing namspace

--- test/count_test.cpp
+++ test/count_test.cpp
@@ -17,9 +17,8 @@ template <std::size_t... List>
 using size_t_list = brigand::integral_list<std::size_t, List...>;
 using full_list = size_t_list<0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11>;
 
-using l1 = brigand::count_if<full_list, brigand::bind<is_odd, _1>>;
-using l2 = brigand::count_if<full_list, lazy_is_odd<_1>>;
+using l1 = brigand::count_if<full_list, brigand::bind<is_odd, brigand::_1>>;
+using l2 = brigand::count_if<full_list, lazy_is_odd<brigand::_1>>;
 
 static_assert(l1::value == 6, "eager count_if error");
 static_assert(l2::value == 6, "lazy count_if error");
-
