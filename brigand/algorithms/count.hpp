e6be14a2e4aeed27e9d0d537abaeeee446a8ef41
Odin Holmes <holmes.odin@gmail.com>
2016-08-16 12:23:25 +0200

    made MSVC work around

--- brigand/algorithms/count.hpp
+++ brigand/algorithms/count.hpp
@@ -5,19 +5,40 @@
   (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
 =================================================================================================**/
 #pragma once
-#include <type_traits>
-#include <brigand/types/integer.hpp>
 #include <brigand/functions/lambda.hpp>
+#include <brigand/types/integer.hpp>
+#include <type_traits>
 
 namespace brigand
 {
-	namespace detail {
-		constexpr std::size_t count_bools(bool const *const begin, bool const *const end,
-			std::size_t n)
-		{
-			return begin == end ? n : detail::count_bools(begin + 1, end, n + *begin);
-		}
+namespace detail
+{
+	constexpr std::size_t count_bools(bool const * const begin, bool const * const end,
+		std::size_t n)
+	{
+		return begin == end ? n : detail::count_bools(begin + 1, end, n + *begin);
 	}
+
+    template <bool... Bs>
+    struct template_count_bools
+    {
+        using type = ::brigand::size_t<0>;
+    };
+    template <bool B, bool... Bs>
+    struct template_count_bools<B, Bs...>
+    {
+        using type = ::brigand::size_t<B + template_count_bools<Bs...>::type::value>;
+    };
+    template <bool B1, bool B2, bool B3, bool B4, bool B5, bool B6, bool B7, bool B8, bool B9,
+              bool B10, bool B11, bool B12, bool B13, bool B14, bool B15, bool B16, bool... Bs>
+    struct template_count_bools<B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12, B13, B14, B15,
+                                B16, Bs...>
+    {
+        using type =
+            ::brigand::size_t<B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9 + B10 + B11 + B12 + B13 +
+                              B14 + B15 + B16 + template_count_bools<Bs...>::type::value>;
+    };
+}
 namespace lazy
 {
     template <typename List, typename Pred>
@@ -25,11 +46,13 @@ namespace lazy
     {
     };
 
-    template <template<typename...> class S, typename Pred>
+    template <template <typename...> class S, typename Pred>
     struct count_if<S<>, Pred>
     {
         using type = ::brigand::size_t<0>;
     };
+
+#ifdef __GNUC__  //not MSVC
 
 	template <template<typename...> class S, typename... Ts, typename Pred>
 	struct count_if<S<Ts...>, Pred>
@@ -38,19 +61,26 @@ namespace lazy
 		using type = brigand::size_t<::brigand::detail::count_bools(s_v, s_v + sizeof...(Ts), 0u)>;
 	};
 
-	template <template<typename...> class S, typename... Ts, template<typename...> class F>
-	struct count_if<S<Ts...>, bind<F,_1>>
-	{
+    template <template <typename...> class S, typename... Ts, template <typename...> class F>
+    struct count_if<S<Ts...>, bind<F, _1>>
+    {
 		static constexpr bool s_v[] = { F<Ts>::value... };
 		using type = brigand::size_t<::brigand::detail::count_bools(s_v, s_v + sizeof...(Ts), 0u)>;
-	};
+    };
 
-	template <template<typename...> class S, typename... Ts, template<typename...> class F>
-	struct count_if<S<Ts...>, F<_1>>
-	{
+    template <template <typename...> class S, typename... Ts, template <typename...> class F>
+    struct count_if<S<Ts...>, F<_1>>
+    {
 		static constexpr bool s_v[] = { F<Ts>::type::value... };
 		using type = brigand::size_t<::brigand::detail::count_bools(s_v, s_v + sizeof...(Ts), 0u)>;
+    };
+#else
+	template <template <typename...> class S, typename... Ts, typename Pred>
+	struct count_if<S<Ts...>, Pred> : ::brigand::detail::template_count_bools<::brigand::apply<Pred, Ts>::value...>
+	{
 	};
+
+#endif
 }
 
 template <typename List, typename Pred>
