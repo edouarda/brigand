c30ce98e3a5bf5d35e355d673eefd72c49905c66
Edouard A <edouard@quasardb.net>
2016-08-20 21:59:34 +0200

    Close #168 - Add specific macros to detect VS2015 and VS2013. Ensures that clang on VS will not be detected as MSVC.

--- brigand/config.hpp
+++ brigand/config.hpp
@@ -8,11 +8,14 @@ Distributed under the Boost Software License, Version 1.0.
 
 // configuration macros for brigand
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && !defined(__GNUC__) && !defined(__clang__)
 #define BRIGAND_COMP_MSVC
+#if _MSC_VER == 1900
+#define BRIGAND_COMP_MSVC_2015
+#elif _MSC_VER == 1800
+#define BRIGAND_COMP_MSVC_2013
 #endif
-
-#ifdef __GNUC__
+#elif __GNUC__
 #ifndef __clang__
 #define BRIGAND_COMP_GCC
 #else
