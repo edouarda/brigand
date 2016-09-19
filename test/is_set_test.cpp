f714df1c2c8da04c3ee60fe1f04275e0ddbafa00
jonathan poelen <jonathan.poelen@gmail.com>
2016-09-19 23:52:36 +0200

    fix #175 - is_set with msvc 2013

--- test/is_set_test.cpp
+++ test/is_set_test.cpp
@@ -4,7 +4,7 @@
 
 static_assert(!brigand::is_set<int, int, int>::value, "error in brigand::is_set");
 static_assert(!brigand::is_set<int, int>::value, "error in brigand::is_set");
-//fails with msvc:  static_assert(!brigand::is_set<void, int&, int, int>::value, "error in brigand::is_set");
+static_assert(!brigand::is_set<void, int&, int, int>::value, "error in brigand::is_set");
 static_assert(brigand::is_set<int>::value, "error in brigand::is_set");
 static_assert(brigand::is_set<>::value, "error in brigand::is_set");
 static_assert(brigand::is_set<int, short>::value, "error in brigand::is_set");
