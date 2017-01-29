7ed748337c58c860bd0bb7a08b61fb152361450e
jonathan poelen <jonathan.poelen@gmail.com>
2017-01-29 01:50:48 +0100

    optimized is_set
    ```cpp
    
    using list = brigand::wrap<brigand::filled_list<int, SIZE>, brigand::is_set>;
    ```
    
    gcc-6.2.0:
    
    Size = |      10     |      50     |     100     |     200     |     400
    ------ | ----------- | ----------- | ----------- | ----------- | -----------
    before | 0.04s - 19M | 0.05s - 20M | 0.06s - 21M | 0.07s - 23M | 0.13s - 27M
    after  | 0.04s - 19M | 0.04s - 19M | 0.05s - 19M | 0.06s - 20M | 0.11s - 21M
    gain   |   0%  -  1% |  20%  -  3% |  16%  -  7% |  14%  - 12% |  15%  - 20%
    
    clang-3.9.0:
    
    Size = |      10     |      50     |     100     |     200     |     400
    ------ | ----------- | ----------- | ----------- | ----------- | -----------
    before | 0.08s - 45M | 0.08s - 45M | 0.08s - 46M | 0.09s - 47M | 0.11s - 48M
    after  | 0.08s - 45M | 0.08s - 45M | 0.08s - 45M | 0.09s - 46M | 0.10s - 47M
    gain   |   0%  -  0% |   0%  -  0% |   0%  -  1% |   0%  -  1% |   9%  -  2%
    
    ```cpp
    
    using list = brigand::wrap<brigand::append<brigand::filled_list<int, SIZE/2>, brigand::range<int, 0, SIZE/2>>, brigand::is_set>;
    ```
    
    gcc-6.2.0:
    
    Size = |      10     |      50     |     100     |     200     |     400
    ------ | ----------- | ----------- | ----------- | ----------- | -----------
    before | 0.04s - 19M | 0.05s - 20M | 0.06s - 22M | 0.07s - 24M | 0.11s - 29M
    after  | 0.04s - 19M | 0.05s - 19M | 0.05s - 20M | 0.06s - 20M | 0.08s - 22M
    gain   |   0%  -  1% |   0%  -  5% |  16%  -  9% |  14%  - 15% |  27%  - 25%
    
    clang-3.9.0:
    
    Size = |      10     |      50     |     100     |     200     |     400
    ------ | ----------- | ----------- | ----------- | ----------- | -----------
    before | 0.08s - 45M | 0.09s - 46M | 0.09s - 46M | 0.10s - 47M | 0.12s - 49M
    after  | 0.08s - 45M | 0.08s - 45M | 0.09s - 45M | 0.09s - 46M | 0.10s - 48M
    gain   |   0%  -  0% |  11%  -  1% |   0%  -  0% |  10%  -  2% |  16%  -  3%
    
    ```cpp
    
    using list = brigand::wrap<brigand::range<int, 0, SIZE>, brigand::is_set>;
    ```
    
    gcc-6.2.0:
    
    Size = |      10     |      50     |     100     |     200     |     400
    ------ | ----------- | ----------- | ----------- | ----------- | -----------
    before | 0.04s - 19M | 0.06s - 21M | 0.07s - 24M | 0.11s - 30M | 0.21s - 48M
    after  | 0.04s - 19M | 0.05s - 19M | 0.05s - 19M | 0.05s - 20M | 0.07s - 22M
    gain   |   0%  -  2% |  16%  -  9% |  28%  - 18% |  54%  - 32% |  66%  - 54%
    
    clang-3.9.0:
    
    Size = |      10     |      50     |     100     |     200     |     400
    ------ | ----------- | ----------- | ----------- | ----------- | -----------
    before | 0.08s - 45M | 0.09s - 46M | 0.12s - 47M | 0.20s - 48M | 0.52s - 52M
    after  | 0.07s - 45M | 0.08s - 45M | 0.08s - 45M | 0.09s - 46M | 0.09s - 47M
    gain   |  12%  -  0% |  11%  -  1% |  33%  -  2% |  55%  -  4% |  82%  -  9%

--- brigand/algorithms/is_set.hpp
+++ brigand/algorithms/is_set.hpp
@@ -8,77 +8,32 @@
 
 #include <brigand/sequences/range.hpp>
 #include <brigand/types/type.hpp>
+#include <brigand/types/bool.hpp>
 
 namespace brigand
 {
 namespace detail
 {
   template<class, class T> struct unique_x_t
-#ifdef BRIGAND_COMP_MSVC_2013
-  { operator T (); };
-#elif defined(BRIGAND_COMP_GCC)
-  : type_<T> // better with gcc, very bad with clang when the list contains many same elements
+  : type_<T>
   {};
-#else
-  { operator type_<T> (); };
-#endif
 
   template<class Ints, class... Ts>
-  struct is_set_impl;
+  struct is_set_cont;
 
   template<>
-  struct is_set_impl<list<>>
+  struct is_set_cont<list<>>
   {
     using type = std::true_type;
   };
 
-  inline std::true_type true_fn(...);
-
   template<class... Ints, class... Ts>
-  struct is_set_impl<list<Ints...>, Ts...>
+  struct is_set_cont<list<Ints...>, Ts...>
+  : unique_x_t<Ints, Ts>...
   {
-    struct Pack : unique_x_t<Ints, Ts>... {};
-
-    template<class... Us>
-    static auto is_set(Us...) -> decltype(true_fn(static_cast<Us>(Pack())...));
-    static std::false_type is_set(...);
-
-#ifdef BRIGAND_COMP_MSVC_2013
-    using type = decltype(is_set(Ts()...));
-#else
-    using type = decltype(is_set(type_<Ts>()...));
-#endif
   };
-
-#ifdef BRIGAND_COMP_MSVC_2013
-  template<class> struct qrref {};
-  template<class> struct qref {};
-  template<class> struct qnoref {};
-
-  template<class T>
-  struct msvc_quali_ref
-  {
-    using type = qnoref<T>;
-  };
-
-  template<class T>
-  struct msvc_quali_ref<T&>
-  {
-    using type = qref<T>;
-  };
-
-  template<class T>
-  struct msvc_quali_ref<T&&>
-  {
-    using type = qrref<T>;
-  };
-#endif
 }
 
   template<class... Ts>
-#ifdef BRIGAND_COMP_MSVC_2013
-  using is_set = typename detail::is_set_impl<range<int, 0, sizeof...(Ts)>, detail::msvc_quali_ref<Ts>...>::type;
-#else
-  using is_set = typename detail::is_set_impl<range<int, 0, sizeof...(Ts)>, Ts...>::type;
-#endif
+  using is_set = bool_<sizeof(detail::is_set_cont<range<int, 0, sizeof...(Ts)>, Ts...>) == 1>;
 }
