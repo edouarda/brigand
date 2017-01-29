cb460201a33bf35e17c769e3d3545631f6063d91
jonathan poelen <jonathan.poelen@gmail.com>
2017-01-29 02:47:52 +0100

    old is_set implementation with msvc

--- brigand/algorithms/is_set.hpp
+++ brigand/algorithms/is_set.hpp
@@ -12,20 +12,74 @@
 
 namespace brigand
 {
+
+#ifdef BRIGAND_COMP_MSVC_2013
+
 namespace detail
 {
   template<class, class T> struct unique_x_t
+  { operator T (); };
+
+  template<class Ints, class... Ts>
+  struct is_set_impl;
+
+  template<>
+  struct is_set_impl<list<>>
+  {
+    using type = std::true_type;
+  };
+
+  inline std::true_type true_fn(...);
+
+  template<class... Ints, class... Ts>
+  struct is_set_impl<list<Ints...>, Ts...>
+  {
+    struct Pack : unique_x_t<Ints, Ts>... {};
+
+    template<class... Us>
+    static auto is_set(Us...) -> decltype(true_fn(static_cast<Us>(Pack())...));
+    static std::false_type is_set(...);
+
+    using type = decltype(is_set(Ts()...));
+  };
+
+  template<class> struct qrref {};
+  template<class> struct qref {};
+  template<class> struct qnoref {};
+
+  template<class T>
+  struct msvc_quali_ref
+  {
+    using type = qnoref<T>;
+  };
+
+  template<class T>
+  struct msvc_quali_ref<T&>
+  {
+    using type = qref<T>;
+  };
+
+  template<class T>
+  struct msvc_quali_ref<T&&>
+  {
+    using type = qrref<T>;
+  };
+}
+
+  template<class... Ts>
+  using is_set = typename detail::is_set_impl<range<std::size_t, 0, sizeof...(Ts)>, detail::msvc_quali_ref<Ts>...>::type;
+
+#else
+
+namespace detail
+{
+  template<class, class T>
+  struct unique_x_t
   : type_<T>
   {};
 
   template<class Ints, class... Ts>
   struct is_set_cont;
-
-  template<>
-  struct is_set_cont<list<>>
-  {
-    using type = std::true_type;
-  };
 
   template<class... Ints, class... Ts>
   struct is_set_cont<list<Ints...>, Ts...>
@@ -35,5 +89,7 @@ namespace detail
 }
 
   template<class... Ts>
-  using is_set = bool_<sizeof(detail::is_set_cont<range<int, 0, sizeof...(Ts)>, Ts...>) == 1>;
+  using is_set = bool_<sizeof(detail::is_set_cont<range<std::size_t, 0, sizeof...(Ts)>, Ts...>) == 1>;
+
+#endif
 }
