c632b154a29bd55eac911fe482e28505a92954c4
Roland Schulz <roland.schulz@intel.com>
2017-03-03 13:44:38 -0800

    Fix compile with Intel compiler
    Tested with 2017u2 and 2016u3

--- brigand/algorithms/split_at.hpp
+++ brigand/algorithms/split_at.hpp
@@ -6,6 +6,7 @@ Distributed under the Boost Software License, Version 1.0.
 =================================================================================================**/
 #pragma once
 
+#include <brigand/config.hpp>
 #include <brigand/sequences/clear.hpp>
 
 namespace brigand
