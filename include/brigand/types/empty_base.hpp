897d75d2783ca0c7c614aa28153f18afed7a9e08
Nils Deppe <nd357@cornell.edu>
2017-06-24 01:08:44 -0400

    Change copyright header to use Doxygen (#248)

--- include/brigand/types/empty_base.hpp
+++ include/brigand/types/empty_base.hpp
@@ -1,9 +1,9 @@
-/*==================================================================================================
-  Copyright (c) 2015 Edouard Alligand and Joel Falcou
+/*!
+@file
 
-  Distributed under the Boost Software License, Version 1.0.
-  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-=================================================================================================**/
+@copyright Edouard Alligand and Joel Falcou 2015-2017
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
 #ifndef BOOST_BRIGAND_TYPES_EMPTY_BASE_HPP
 #define BOOST_BRIGAND_TYPES_EMPTY_BASE_HPP
 
--- include/standalone/brigand.hpp
+++ include/standalone/brigand.hpp
@@ -1,8 +1,9 @@
-/*==================================================================================================
-  Copyright (c) 2015 Edouard Alligand and Joel Falcou
-  Distributed under the Boost Software License, Version 1.0.
-  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
-=================================================================================================**/
+/*!
+@file
+
+@copyright Edouard Alligand and Joel Falcou 2015-2017
+(See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
 #ifndef BRIGAND_HPP_INCLUDED
 #define BRIGAND_HPP_INCLUDED
 namespace brigand
