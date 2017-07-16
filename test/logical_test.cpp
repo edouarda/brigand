5422348a1f6ef5fb5735bae0324c83492b91dd4d
Alexander Huszagh <ahuszagh@gmail.com>
2017-07-16 03:43:34 -0700

    Added variadic and_/or_ and tests for 0-arg, 1-arg, 3-arg, and N-arg cases (#252)

--- test/logical_test.cpp
+++ test/logical_test.cpp
@@ -7,6 +7,12 @@ static_assert(brigand::not_<brigand::false_type>::value == true, "invalid not re
 static_assert(brigand::not_<brigand::not_<brigand::true_type>>::value == true,
               "invalid not result");
 
+static_assert(brigand::and_<>::value == true,
+              "invalid not result");
+static_assert(brigand::and_<brigand::false_type>::value == false,
+              "invalid not result");
+static_assert(brigand::and_<brigand::true_type>::value == true,
+              "invalid not result");
 static_assert(brigand::and_<brigand::false_type, brigand::false_type>::value == false,
               "invalid and result");
 static_assert(brigand::and_<brigand::false_type, brigand::true_type>::value == false,
@@ -15,10 +21,73 @@ static_assert(brigand::and_<brigand::true_type, brigand::false_type>::value == f
               "invalid and result");
 static_assert(brigand::and_<brigand::true_type, brigand::true_type>::value == true,
               "invalid and result");
+static_assert(brigand::and_<brigand::false_type, brigand::false_type, brigand::false_type>::value == false,
+              "invalid not result");
+static_assert(brigand::and_<brigand::true_type, brigand::false_type, brigand::false_type>::value == false,
+              "invalid not result");
+static_assert(brigand::and_<brigand::true_type, brigand::true_type, brigand::false_type>::value == false,
+              "invalid not result");
+static_assert(brigand::and_<brigand::true_type, brigand::true_type, brigand::true_type>::value == true,
+              "invalid not result");
 
 static_assert(brigand::and_<brigand::not_<brigand::true_type>, brigand::false_type>::value == false,
               "invalid and/not result");
 
+static_assert(brigand::and_<brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::false_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type>::value == false,
+              "invalid or result");
+
+static_assert(brigand::and_<brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type,
+              brigand::true_type, brigand::true_type>::value == true,
+              "invalid or result");
+
+
+static_assert(brigand::or_<>::value == false,
+              "invalid not result");
+static_assert(brigand::or_<brigand::false_type>::value == false,
+              "invalid not result");
+static_assert(brigand::or_<brigand::true_type>::value == true,
+              "invalid not result");
 static_assert(brigand::or_<brigand::false_type, brigand::false_type>::value == false,
               "invalid or result");
 static_assert(brigand::or_<brigand::false_type, brigand::true_type>::value == true,
@@ -26,6 +95,62 @@ static_assert(brigand::or_<brigand::false_type, brigand::true_type>::value == tr
 static_assert(brigand::or_<brigand::true_type, brigand::false_type>::value == true,
               "invalid or result");
 static_assert(brigand::or_<brigand::true_type, brigand::true_type>::value == true,
+              "invalid or result");
+static_assert(brigand::or_<brigand::false_type, brigand::false_type, brigand::false_type>::value == false,
+              "invalid not result");
+static_assert(brigand::or_<brigand::true_type, brigand::false_type, brigand::false_type>::value == true,
+              "invalid not result");
+static_assert(brigand::or_<brigand::true_type, brigand::true_type, brigand::false_type>::value == true,
+              "invalid not result");
+static_assert(brigand::or_<brigand::true_type, brigand::true_type, brigand::true_type>::value == true,
+              "invalid not result");
+
+static_assert(brigand::or_<brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type>::value == false,
+              "invalid or result");
+
+static_assert(brigand::or_<brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::true_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type,
+              brigand::false_type, brigand::false_type>::value == true,
               "invalid or result");
 
 static_assert(brigand::xor_<brigand::false_type, brigand::false_type>::value == false,
